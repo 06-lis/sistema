aa19c9049e21d7b3ce7de6600ac75944dbeba617
06-lis <munozherbas@gmail.com>
2024-12-17 14:04:11 -0400

    Actualización del proyecto: descripción breve del cambio

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\AlmacenController;
+use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\TipoEmpleadoController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\VentaController;
+use App\Http\Controllers\CompraController;
+use App\Http\Controllers\DetalleAlmacenController;
+use App\Http\Controllers\DetalleCompraController;
+use App\Http\Controllers\DetalleVentaController;
+use App\Http\Controllers\InformeController;
+
+
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+Auth::routes();
+
+
+
+Route::middleware('auth')->group(function () {
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+
+
+Route::resource('clientes', ClienteController::class);
+Route::resource('proveedores', ProveedorController::class);
+Route::resource('categorias', CategoriaController::class);
+Route::resource('tipoEmpleados', TipoEmpleadoController::class);
+Route::resource('logins', LoginController::class);
+Route::resource('almacens', AlmacenController::class);
+Route::resource('empleados', EmpleadoController::class);
+Route::resource('productos', ProductoController::class);
+Route::resource('ventas', VentaController::class);
+Route::resource('compras', CompraController::class);
+Route::resource('detalleAlmacens', DetalleAlmacenController::class);
+Route::resource('detalleCompras', DetalleCompraController::class);
+Route::resource('detalleVentas', DetalleVentaController::class);
+route::get('/detalleVenta',[DetalleVentaController::class,'index'])->name('detalleVenta.index');
+Route::resource('informes', InformeController::class);
+
+});
+
+
